30221524273ef579a8c0d5d4125499f22c1e8211
bebokaka99 <nq20k2@gmail.com>
2025-11-21 14:48:52 +0700

    reponsive

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,15 @@ export default {
   ],
   darkMode: "class", 
   theme: {
+    // TỐI ƯU CONTAINER CHO MOBILE
+    container: {
+      center: true, // Luôn căn giữa
+      padding: {
+        DEFAULT: '1rem', // Mobile: Cách lề 16px
+        sm: '2rem',      // Tablet: Cách lề 32px
+        lg: '4rem',      // PC: Cách lề 64px
+      },
+    },
     extend: {
       colors: {
         "primary": "#1313ec",
@@ -13,7 +22,6 @@ export default {
         "background-dark": "#101022", 
       },
       fontFamily: {
-        // Đặt Quicksand cho cả 2 loại
         "display": ["Quicksand", "sans-serif"], 
         "heading": ["Quicksand", "sans-serif"], 
       },
